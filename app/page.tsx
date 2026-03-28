80ed16b7307c904785d121e03b5c1e0fd6c97f4e
Avinash Raut <36771262+avinaashraut@users.noreply.github.com>
2026-03-28 15:43:05 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <main style={{ padding: 40 }}>
       <h1>My Portfolio</h1>
-      <p>Site is starting…</p>
+
+      <div style={{ marginTop: 20 }}>
+        <Link href="/blog">Go to Blog</Link>
+      </div>
     </main>
   )
 }
